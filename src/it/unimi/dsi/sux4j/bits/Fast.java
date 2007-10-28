3e62f363d8e9d97192544377941c1197f1194398
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2007-10-28 13:15:24 +0000

    Tweaking

--- src/it/unimi/dsi/sux4j/bits/Fast.java
+++ src/it/unimi/dsi/sux4j/bits/Fast.java
@@ -1,5 +1,7 @@
 package it.unimi.dsi.sux4j.bits;
 
+import java.util.Arrays;
+
 /*		 
  * Sux4J: Succinct data structures for Java
  *
@@ -21,8 +23,6 @@ package it.unimi.dsi.sux4j.bits;
  *
  */
 
-import static it.unimi.dsi.mg4j.util.Fast.mostSignificantBit;
-import it.unimi.dsi.mg4j.io.InputBitStream;
 
 /** All-purpose optimised static-method container class.
  *
@@ -47,13 +47,31 @@ public final class Fast {
 		return mostSignificantBit( x - 1 ) + 1;
 	}
 
+	/** Returns the number of bits that are necessary to encode the argument.
+	 * 
+	 * @param x an integer.
+	 * @return the number of bits that are necessary to encode <code>x</code>.
+	 */
 	public static int length( final int x ) {
 		return x == 0 ? 1 : mostSignificantBit( x ) + 1;
 	}
 
+	/** Returns the number of bits that are necessary to encode the argument.
+	 * 
+	 * @param x a long.
+	 * @return the number of bits that are necessary to encode <code>x</code>.
+	 */
 	public static int length( final long x ) {
 		return x == 0 ? 1 : mostSignificantBit( x ) + 1;
 	}
+
+	/** Returns the number of bits set to one in a long.
+	 * 
+	 * <p>This method implements a classical broadword algorithm. 
+	 * 
+	 * @param x a long.
+	 * @return the number of bits set to one in <code>x</code>.
+	 */
 
 	public static int count( final long x ) {
 		long byteSums = x - ( ( x & 0xa * ONES_STEP_4 ) >>> 1 );
@@ -62,9 +80,16 @@ public final class Fast {
         return (int)( byteSums * ONES_STEP_8 >>> 56 );
 	}
 	
+	/** Returns the most significant bit of a long.
+	 * 
+	 * <p>This method implements Gerth Brodal's broadword algorithm. On 64-bit architectures
+	 * it is an order of magnitude faster than standard bit-fiddling techniques.
+	 * 
+	 * @param x a long.
+	 * @return the most significant bit of <code>x</code>, of <code>x</code> is nonzero; &minus;1, otherwise.
+	 */
 	public static int mostSignificantBit( long x ) {
 		if ( x == 0 ) return -1;
-		//System.err.println( "x: "+ x );
 		
 		int msb = 0;
 		
@@ -78,35 +103,44 @@ public final class Fast {
 			msb += ( 1 << 4 );
 		}
 		
+		// We have now reduced the problem to finding the msb in a 16-bit word.
+		
 		x |= x << 16;
 		x |= x << 32;
 		
-		//System.err.println( "x2: " + Long.toBinaryString( x ) );
-		
 		final long y = x & 0xFF00F0F0CCCCAAAAL;
-		//System.err.println( "y: " + Long.toBinaryString( y  ) );
 		
 		long t = 0x8000800080008000L & ( y | (( y | 0x8000800080008000L ) - ( x ^ y )));
-		
-		//System.err.println( "t: " + Long.toBinaryString( t  ) );
 		
 		t |= t << 15;
 		t |= t << 30;
 		t |= t << 60;
 		
-		//System.err.println( "msb:" + msb + " t2: " + Long.toBinaryString( t  ) );
-
 		return (int)( msb + ( t >>> 60 ) );
+	}
+	
+	final private static int[] LSB_TABLE = { 
+		0, 1, 56, 2, 57, 49, 28, 3, 61, 58, 42, 50, 38, 29, 17, 4, 62, 47, 59, 36, 45, 43, 51, 22, 53, 39, 33, 30, 24, 18, 12, 5, 63, 55, 48, 27, 60, 41, 37, 16, 46, 35, 44, 21, 52, 32, 23, 11, 54, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6
+	};
+	
+	/** Returns the least significant bit of a long.
+	 * 
+	 * 
+	 * @param x a long.
+	 * @return the least significant bit of <code>x</code>, of <code>x</code> is nonzero; &minus;1, otherwise.
+	 */
+	public static int leastSignificantBit( long x ) {
+		return LSB_TABLE[ (int)( ( ( x & -x ) * 0x03f79d71b4ca8b09L ) >>> 58 ) ]; 
 	}
 	
 	public static void main( final String a[] ) {
 		final long n = Long.parseLong( a[ 0 ] );
 		final long incr = Long.MAX_VALUE / ( n / 2 );
-		
+				
 		long start, elapsed;
 		
 		for( int k = 10; k-- !=0;  ) {
-			System.out.print( "Broadword: " );
+			System.out.print( "Broadword msb: " );
 			
 			start = System.currentTimeMillis();
 			for( long i = n, v = 0; i-- != 0; ) mostSignificantBit( v += incr );
@@ -114,7 +148,7 @@ public final class Fast {
 
 			System.out.println( "elapsed " + elapsed + ", " + ( 1000000.0 * elapsed / n ) + " ns/call" );
 
-			System.out.print( "Dichotomous: " );
+			System.out.print( "Dichotomous msb: " );
 			
 			start = System.currentTimeMillis();
 			for( long i = n, v = 0; i-- != 0; ) it.unimi.dsi.mg4j.util.Fast.mostSignificantBit( v += incr );
@@ -122,13 +156,38 @@ public final class Fast {
 
 			System.out.println( "elapsed " + elapsed + ", " + ( 1000000.0 * elapsed / n ) + " ns/call" );
 
-			System.out.print( "java.lang: " );
+			System.out.print( "java.lang msb: " );
 			
 			start = System.currentTimeMillis();
 			for( long i = n, v = 0; i-- != 0; ) Long.numberOfLeadingZeros( v += incr );
 			elapsed = System.currentTimeMillis() - start;
 
 			System.out.println( "elapsed " + elapsed + ", " + ( 1000000.0 * elapsed / n ) + " ns/call" );
+
+			System.out.print( "Multiplication/lookup lsb: " );
+			
+			start = System.currentTimeMillis();
+			for( long i = n, v = 0; i-- != 0; ) leastSignificantBit( v += incr );
+			elapsed = System.currentTimeMillis() - start;
+
+			System.out.println( "elapsed " + elapsed + ", " + ( 1000000.0 * elapsed / n ) + " ns/call" );
+
+			System.out.print( "Byte-by-byte lsb: " );
+			
+			start = System.currentTimeMillis();
+			for( long i = n, v = 0; i-- != 0; ) it.unimi.dsi.mg4j.util.Fast.leastSignificantBit( v += incr );
+			elapsed = System.currentTimeMillis() - start;
+
+			System.out.println( "elapsed " + elapsed + ", " + ( 1000000.0 * elapsed / n ) + " ns/call" );
+
+			System.out.print( "java.lang lsb: " );
+			
+			start = System.currentTimeMillis();
+			for( long i = n, v = 0; i-- != 0; ) Long.numberOfTrailingZeros( v += incr );
+			elapsed = System.currentTimeMillis() - start;
+
+			System.out.println( "elapsed " + elapsed + ", " + ( 1000000.0 * elapsed / n ) + " ns/call" );
+
 		}
 
 	}
