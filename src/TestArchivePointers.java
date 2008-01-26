4d7665b699e1b67b022d368468c5f90fc7919dee
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-01-26 12:42:19 +0000

    Tweaking

--- src/TestArchivePointers.java
+++ src/TestArchivePointers.java
@@ -1,6 +1,8 @@
 import it.unimi.dsi.fastutil.longs.AbstractLongIterator;
+import it.unimi.dsi.fastutil.io.*;
 import it.unimi.dsi.mg4j.io.InputBitStream;
 import it.unimi.dsi.sux4j.bits.SparseSelect;
+import it.unimi.dsi.sux4j.bits.*;
 
 import java.io.IOException;
 
@@ -12,15 +14,20 @@ public class TestArchivePointers {
 		final InputBitStream skips = new InputBitStream( a[ 0 ] );
 		final long n = Long.parseLong( a[ 1 ] );
 		final long m = Long.parseLong( a[ 2 ] );
-
+		final long[] doc = new long[ (int)m ];
+		final int[] currDoc = new int[ 1 ];
+		final int[] p = new int[ 1 ];
+		
 		SparseSelect sparse = new SparseSelect( n, m, new AbstractLongIterator() {
 			int i = 0;
 			long curr = 0;
-
+			
 			public long nextLong() {
 				i++;
 				try {
-					skips.readZeta(3);
+					final int s = skips.readZeta(3);
+					doc[ p[0]++ ] = s;
+					
 					return curr += skips.readLongGamma();
 				}
 				catch ( IOException e ) {
@@ -32,7 +39,29 @@ public class TestArchivePointers {
 				return i < m;
 			}
 		} );
+
+		skips.position( 0 );
+		long numDocs = doc[ p[0] - 1 ] + 1;
+
+//		SimpleSelectZero sparse2 = new SimpleSelectZero( currDoc[ 0 ] + 1, p[0], it.unimi.dsi.fastutil.longs.LongArrayList.wrap( doc, p[0] ).iterator() );
+		LongArrayBitVector v = LongArrayBitVector.getInstance( numDocs ).length( numDocs );
+		for( int i = p[0]; i-- != 0; ) v.set( doc[ i ] );
+		v.flip();
+		SparseRank sparse2 = new SparseRank( v );
+
+		skips.position( 0 );
+		long curr = 0, skip;
+		for( int i = 0; i < m; i++ ) {
+			skip = skips.readZeta(3);
+			assert i == sparse2.rankZero( skip ) : "At " + i + ": (skip=" + skip + "):" + i + " != " + sparse2.rankZero( skip );
+			curr += skips.readLongGamma();
+			assert curr == sparse.select( i ) : "At " + i + ": " + curr + " != " + sparse.select( i );
+		}
 		
 		System.out.println( "Bits per pointer: " + (double)sparse.numBits() / m );
+		System.out.println( "Bits per identifier: " + (double)sparse2.numBits() / numDocs );
+
+		BinIO.storeObject( sparse, "uk.offsets" );
+		BinIO.storeObject( sparse2, "uk.docs" );
 	}
 }
