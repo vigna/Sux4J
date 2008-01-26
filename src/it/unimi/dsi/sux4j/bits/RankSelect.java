90d2aa9a58b14c186711b8a5ecacf21f4f707785
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-01-26 12:50:04 +0000

    Tweaking

--- src/it/unimi/dsi/sux4j/bits/RankSelect.java
+++ src/it/unimi/dsi/sux4j/bits/RankSelect.java
@@ -2,22 +2,42 @@ package it.unimi.dsi.sux4j.bits;
 
 import java.io.Serializable;
 
-/** A container for associated rank/select(zero) structures.
+/** A serialisation-oriented container for associated rank/select(zero) structures.
+ *  
+ *  <p>Since structures in Sux4J serialise all contained data, including, if necessary, the underlying bit vector,
+ *  serialising separately a rank and a select structure might result in storing the underlying bit
+ *  vector twice. This class provide a simple solution by providing one-shot serialisation of
+ *  all structures related to a bit vector. As a commodity, it provides also delegate methods, albeit
+ *  the suggested usage is deserialisation and extraction of non-<code>null</code> structures.  
  *  
  */
 public class RankSelect implements Rank, Select, SelectZero, Serializable {
 
 	private static final long serialVersionUID = 1L;
+	/** A rank structure, or <code>null</code>. */
 	public final Rank rank;
+	/** A select structure, or <code>null</code>. */
 	public final Select select;
+	/** A zero-select structure, or <code>null</code>. */
 	public final SelectZero selectZero;
 
+	/** Creates a new rank/select container using the given structures.
+	 * 
+	 * @param rank a rank structure, or <code>null</code>. 
+	 * @param select a select structure, or <code>null</code>. 
+	 * @param selectZero a zero-select structure, or <code>null</code>.
+	 */
 	public RankSelect( final Rank rank, final Select select, final SelectZero selectZero ) {
 		this.rank = rank;
 		this.select = select;
 		this.selectZero = selectZero;
 	}
 	
+	/** Creates a new rank/select container without zero selection using the given structures.
+	 * 
+	 * @param rank a rank structure, or <code>null</code>. 
+	 * @param select a select structure, or <code>null</code>. 
+	 */
 	public RankSelect( final Rank rank, final Select select ) {
 		this( rank, select, null );
 	}
