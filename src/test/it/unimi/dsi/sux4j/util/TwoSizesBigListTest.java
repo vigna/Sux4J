04e14c756f08d3e5e566c6bf00ec4a3902593cbc
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-02-01 23:51:06 +0000

    Tweaking

---
+++ src/test/it/unimi/dsi/sux4j/util/TwoSizesBigListTest.java
@@ -0,0 +1,21 @@
+package test.it.unimi.dsi.sux4j.util;
+
+import it.unimi.dsi.sux4j.bits.LongArrayBitVector;
+import it.unimi.dsi.sux4j.util.LongBigList;
+import it.unimi.dsi.sux4j.util.TwoSizesLongBigList;
+import junit.framework.TestCase;
+
+public class TwoSizesBigListTest extends TestCase {
+	public void testConstruction() {
+		LongBigList l = LongArrayBitVector.getInstance().asLongBigList( 10 );
+		for( int i = 0; i < 1024; i++ ) l.add( i );
+		TwoSizesLongBigList ts = new TwoSizesLongBigList( l, 10 );
+		assertEquals( ts, l );
+
+		l.clear();
+		for( int i = 0; i < 512; i++ ) l.add( 2 );
+		for( int i = 0; i < 512; i++ ) l.add( i );
+		ts = new TwoSizesLongBigList( l, 10 );
+		assertEquals( ts, l );
+	}
+}
