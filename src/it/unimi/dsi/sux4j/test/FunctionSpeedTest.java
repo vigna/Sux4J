76a045c2961d699ee3f9e08c0cda4e7876e9808f
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-03-12 09:04:34 +0000

    Tweaking

--- src/it/unimi/dsi/sux4j/test/FunctionSpeedTest.java
+++ src/it/unimi/dsi/sux4j/test/FunctionSpeedTest.java
@@ -1,12 +1,18 @@
 package it.unimi.dsi.sux4j.test;
 
+import it.unimi.dsi.Util;
+import it.unimi.dsi.bits.LongArrayBitVector;
 import it.unimi.dsi.fastutil.io.BinIO;
 import it.unimi.dsi.fastutil.objects.Object2LongFunction;
 import it.unimi.dsi.io.FileLinesCollection;
+import it.unimi.dsi.lang.MutableString;
+import it.unimi.dsi.sux4j.io.FileLinesList;
 import it.unimi.dsi.sux4j.mph.HollowTrie;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 
 import com.martiansoftware.jsap.FlaggedOption;
@@ -28,6 +34,7 @@ public class FunctionSpeedTest {
 					new FlaggedOption( "bufferSize", JSAP.INTSIZE_PARSER, "64Ki", JSAP.NOT_REQUIRED, 'b',  "buffer-size", "The size of the I/O buffer used to read terms." ),
 					new FlaggedOption( "encoding", ForNameStringParser.getParser( Charset.class ), "UTF-8", JSAP.NOT_REQUIRED, 'e', "encoding", "The term file encoding." ),
 					new Switch( "zipped", 'z', "zipped", "The term list is compressed in gzip format." ),
+					new Switch( "random", 'r', "random", "Do a random test on at most 1 million strings." ),
 					new Switch( "check", 'c', "check", "Check that the term list is mapped to its ordinal positiona." ),
 					new FlaggedOption( "termFile", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'o', "offline", "Read terms from this file (without loading them into core memory) instead of standard input." ),
 					new UnflaggedOption( "function", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The filename for the serialised function." )
@@ -41,25 +48,51 @@ public class FunctionSpeedTest {
 		final Charset encoding = (Charset)jsapResult.getObject( "encoding" );
 		final boolean zipped = jsapResult.getBoolean( "zipped" );
 		final boolean check = jsapResult.getBoolean( "check" );
+		final boolean random = jsapResult.getBoolean( "random" );
 		
 		@SuppressWarnings("unchecked")
 		final Object2LongFunction<? extends CharSequence> function = (Object2LongFunction<? extends CharSequence>)BinIO.loadObject( functionName );
 		final FileLinesCollection flc = new FileLinesCollection( termFile, encoding.name(), zipped );
 
-		for( int k = 10; k-- != 0; ) {
-			final Iterator<? extends CharSequence> i = flc.iterator();
+		if ( random ) {
+			final FileLinesList fll = new FileLinesList( termFile, encoding.name() );
+			final int size = fll.size();
+			int n = Math.min( 1000000, size );
+			final MutableString[] test = new MutableString[ n ];
+			final int step = size / n;
+			for( int i = 0; i < n; i++ ) test[ i ] = fll.get( i * step );
+			Collections.shuffle( Arrays.asList( test ) );
 			
-			long time = -System.currentTimeMillis();
-			int j = 0;
-			long index;
-			while( i.hasNext() ) {
-				index = function.getLong( i.next() );
-				if ( check && index != j ) throw new AssertionError( index + " != " + j ); 
-				if ( j++ % 10000 == 0 ) System.err.print('.');
+			long total = 0;
+			for( int k = 13; k-- != 0; ) {
+				long time = -System.currentTimeMillis();
+				for( int i = 0; i < n; i++ ) {
+					function.getLong( test[ i ] );
+					if ( i++ % 100000 == 0 ) System.out.print('.');
+				}
+				System.out.println();
+				time += System.currentTimeMillis();
+				if ( k < 10 ) total += time;
+				System.out.println( time / 1E3 + "s, " + ( time * 1E3 ) / n + " \u00b5s/item" );
 			}
-			System.err.println();
-			time += System.currentTimeMillis();
-			System.err.println( time / 1E3 + "s, " + ( time * 1E6 ) / j + " ns/item" );
+			System.out.println( "Average: " + Util.format( total / 10E3 ) + "s, " + Util.format( ( total * 1E3 ) / ( 10 * n ) ) + " \u00b5s/item" );
+		}
+		else {
+			for( int k = 10; k-- != 0; ) {
+				final Iterator<? extends CharSequence> i = flc.iterator();
+
+				long time = -System.currentTimeMillis();
+				int j = 0;
+				long index;
+				while( i.hasNext() ) {
+					index = function.getLong( i.next() );
+					if ( check && index != j ) throw new AssertionError( index + " != " + j ); 
+					if ( j++ % 10000 == 0 ) System.err.print('.');
+				}
+				System.err.println();
+				time += System.currentTimeMillis();
+				System.err.println( time / 1E3 + "s, " + ( time * 1E6 ) / j + " ns/item" );
+			}
 		}
 	}
 }
