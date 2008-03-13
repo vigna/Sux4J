00d1855ff43f2eddc6519450335373d86705bbf9
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-03-13 23:57:29 +0000

    Tweaking

---
+++ src/test/it/unimi/dsi/sux4j/util/CompressedLongBigListTest.java
@@ -0,0 +1,26 @@
+package test.it.unimi.dsi.sux4j.util;
+
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.sux4j.util.CompressedLongBigList;
+import junit.framework.TestCase;
+
+public class CompressedLongBigListTest extends TestCase {
+
+	public void testSmall() {
+		LongArrayList l;
+	l = new LongArrayList( new long[] { 0, 0, 0 } );
+		assertEquals( l, new CompressedLongBigList( l.iterator() ) );
+		
+		l = new LongArrayList( new long[] { 0, 1, 0 } );
+		assertEquals( l, new CompressedLongBigList( l.iterator() ) );
+
+		l = new LongArrayList( new long[] { 1, 1, 1 } );
+		assertEquals( l, new CompressedLongBigList( l.iterator() ) );
+		
+		l = new LongArrayList( new long[] { 4, 3, 2 } );
+		assertEquals( l, new CompressedLongBigList( l.iterator() ) );
+
+		l = new LongArrayList( new long[] { 128, 2000, 50000000, 200, 10 } );
+		assertEquals( l, new CompressedLongBigList( l.iterator() ) );
+	}
+}
