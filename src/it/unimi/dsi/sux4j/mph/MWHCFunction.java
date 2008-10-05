62b90e252e3eb787da46e7fe10740f509d471090
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-10-05 23:25:51 +0000

    Tweaking

--- src/it/unimi/dsi/sux4j/mph/MWHCFunction.java
+++ src/it/unimi/dsi/sux4j/mph/MWHCFunction.java
@@ -104,7 +104,7 @@ import com.martiansoftware.jsap.stringparsers.ForNameStringParser;
 public class MWHCFunction<T> extends AbstractObject2LongFunction<T> implements Serializable {
     public static final long serialVersionUID = 1L;
     private static final Logger LOGGER = Util.getLogger( MWHCFunction.class );
-	private static final boolean ASSERTS = true;
+	private static final boolean ASSERTS = false;
 	private static final boolean DEBUG = false;
 		
 	/** The number of elements. */
