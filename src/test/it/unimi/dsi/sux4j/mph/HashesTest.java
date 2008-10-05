9a4f4723573e7cffc5e10981361c40408fe7623f
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2008-10-05 17:04:48 +0000

    Tweaking

---
+++ src/test/it/unimi/dsi/sux4j/mph/HashesTest.java
@@ -0,0 +1,33 @@
+package test.it.unimi.dsi.sux4j.mph;
+
+import java.util.Random;
+
+import it.unimi.dsi.bits.BitVectors;
+import it.unimi.dsi.bits.LongArrayBitVector;
+import it.unimi.dsi.sux4j.mph.Hashes;
+import junit.framework.TestCase;
+
+public class HashesTest extends TestCase {
+
+	
+	public void testPreprocessing() {
+		Random r = new Random( 0 );
+		for ( int l = 0; l < 1000; l++ ) {
+			LongArrayBitVector bv = LongArrayBitVector.getInstance();
+			for( int i = 0; i < l; i++ ) bv.add( r.nextBoolean() );
+			long[][] state = Hashes.preprocessJenkins( bv, 0 );
+			for( int i = 0; i < l; i++ ) {
+				final long[] h = new long[ 3 ];
+				Hashes.jenkins( bv, i, state[ 0 ], state[ 1 ], state[ 2 ], h );
+				assertEquals( "Prefix length " + i, Hashes.jenkins( bv.subVector( 0, i ) ), h[ 0 ] ); 
+			}
+		}
+	}
+	
+	public void test0() {
+		final long[] h = new long[ 3 ];
+		Hashes.jenkins( BitVectors.EMPTY_VECTOR, 0, h );
+		assertEquals( Hashes.jenkins( BitVectors.EMPTY_VECTOR, 0 ), h[ 0 ] );
+		assertEquals( Hashes.jenkins( BitVectors.EMPTY_VECTOR ), h[ 0 ] );
+	}
+}
