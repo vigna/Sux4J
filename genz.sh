0c26d3ad4a043d590a32dda2f59c6ba23bf00d16
Paolo Boldi <paolo.boldi@unimi.it>
2009-03-09 15:34:16 +0000

    Genz fixed for different sed

--- genz.sh
+++ genz.sh
@@ -5,5 +5,6 @@ cp src/it/unimi/dsi/sux4j/bits/SimpleSelect.java src/it/unimi/dsi/sux4j/bits/Sim
 sed -i -e 's/bits\[ /~bits\[ /g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/Select/SelectZero/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/select(/selectZero(/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
-sed -i -e '/numOnes = c/iif ( length % 64 != 0 ) c -= 64 - length % 64;' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
+sed -i -e '/numOnes = c/i\
+if ( length % 64 != 0 ) c -= 64 - length % 64;' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/select implementation/zero-select implementation/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
