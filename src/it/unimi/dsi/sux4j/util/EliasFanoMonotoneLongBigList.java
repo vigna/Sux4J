d5ce7753680f6a4b8600be2c8ea4f68663c6f16e
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-05-06 22:04:41 +0000

    No list

--- src/it/unimi/dsi/sux4j/util/EliasFanoMonotoneLongBigList.java
+++ src/it/unimi/dsi/sux4j/util/EliasFanoMonotoneLongBigList.java
@@ -262,7 +262,7 @@ public class EliasFanoMonotoneLongBigList extends AbstractLongBigList implements
 
 	public long getLong( final long index ) {
 		final int l = Long.SIZE - this.l;
-		final long start = index * l; 
+		final long start = index * this.l; 
 		final int startWord = (int)( start >>> Long.SIZE );
 		final int startBit = (int)( start & ( Long.SIZE - 1 ) );
 		
