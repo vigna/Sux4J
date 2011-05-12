29ff0b5a5fdf376bae0b5a7e14a271ab8e9bc7e3
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2011-05-12 10:05:03 +0000

    Tweaking

---
+++ slow/it/unimi/dsi/sux4j/bits/Rank9SelectSlowTest.java
@@ -0,0 +1,24 @@
+package it.unimi.dsi.sux4j.bits;
+
+
+import static org.junit.Assert.assertEquals;
+import it.unimi.dsi.bits.LongArrayBitVector;
+
+import org.junit.Test;
+
+public class Rank9SelectSlowTest {
+
+	@Test
+	public void testVeryLarge() {
+		LongArrayBitVector v = LongArrayBitVector.getInstance( 2200000000L );
+		for ( int i = 0; i < 2200000000L / 64; i++ )
+			v.append( 0x5555555555555555L, 64 );
+		Rank9 rank9;
+		Select9 select9 = new Select9( rank9 = new Rank9( v ) );
+		for ( int i = 0; i < 1100000000; i++ )
+			assertEquals( i * 2L, select9.select( i ) );
+		for ( int i = 0; i < 1100000000; i++ )
+			assertEquals( i, rank9.rank( i * 2L ) );
+	}
+
+}
