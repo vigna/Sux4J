4d708405b0c1d39d06797db2c98339a8db2806db
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2011-05-14 13:02:51 +0000

    Tweaking

--- slow/it/unimi/dsi/sux4j/mph/HollowTrieMonotoneMinimalPerfectHashFunctionSlowTest.java
+++ slow/it/unimi/dsi/sux4j/mph/HollowTrieMonotoneMinimalPerfectHashFunctionSlowTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 
 public class HollowTrieMonotoneMinimalPerfectHashFunctionSlowTest {
 
-	@Ignore("Runs out of memory")
 	@Test
 	public void testBig() {
 		Iterable<Long> p = LargeLongCollection.getInstance();		
