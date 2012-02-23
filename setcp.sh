5b20ad27df69fd527982b092bb8e70115d04f1d6
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2012-02-23 14:15:54 +0000

    Tweaking

--- setcp.sh
+++ setcp.sh
@@ -11,15 +11,4 @@ else
 	export CLASSPATH=$(ls -1 $sourcedir/$JAR-*.jar | tail -n 1):$CLASSPATH
 fi
 
-export CLASSPATH=$CLASSPATH:/usr/share/java/dsiutils.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/fastutil.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/jsap.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/junit4.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/log4j.jar
-# Indirect dependencies from dsiutils.
-export CLASSPATH=$CLASSPATH:/usr/share/java/jakarta-commons-collections.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/jakarta-commons-configuration.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/jakarta-commons-io.jar
-export CLASSPATH=$CLASSPATH:/usr/share/java/jakarta-commons-lang.jar
-# This is necessary for unit tests (to process the output)
-export CLASSPATH=$CLASSPATH:/usr/share/java/xalan-j2-serializer.jar
+export CLASSPATH=$CLASSPATH:$(\ls -1 $sourcedir/jars/runtime/*.jar | paste -d: -s)
