4bbfe9520f32d9757b7499b635cfe9c42b6923d5
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2015-03-06 14:16:19 +0000

    Tweaking

--- src/it/unimi/dsi/sux4j/util/ZFastTrie.java
+++ src/it/unimi/dsi/sux4j/util/ZFastTrie.java
@@ -3,7 +3,7 @@ package it.unimi.dsi.sux4j.util;
 /*		 
  * Sux4J: Succinct data structures for Java
  *
- * Copyright (C) 2010-2014 Sebastiano Vigna 
+ * Copyright (C) 2010-2015 Sebastiano Vigna 
  *
  *  This library is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU Lesser General Public License as published by the Free
