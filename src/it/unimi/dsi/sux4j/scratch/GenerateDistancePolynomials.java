cb4cd0b38973f3ae688413df040a1c108f85880b
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2017-10-23 08:57:44 +0200

    Tweaking

--- src/it/unimi/dsi/sux4j/scratch/GenerateDistancePolynomials.java
+++ src/it/unimi/dsi/sux4j/scratch/GenerateDistancePolynomials.java
@@ -1,34 +1,24 @@
 package it.unimi.dsi.sux4j.scratch;
 
-import java.util.Scanner;
-
 import it.unimi.dsi.sux4j.mph.solve.Modulo2System;
 import it.unimi.dsi.sux4j.mph.solve.Modulo2System.Modulo2Equation;
 
-public class GenerateDistancePolynomialsXoRoShiRo128 {
+public class GenerateDistancePolynomials {
 	public static int BITS = 128;
 	
 	private static long s0 = -1, s1;
-	private static int A, B, C;
+
+	// 55-14-36	63	x^128 + x^118 + x^117 + x^114 + x^112 + x^109 + x^108 + x^107 + x^106 + x^103 + x^102 + x^101 + x^99 + x^98 + x^96 + x^94 + x^93 + x^92 + x^91 + x^90 + x^89 + x^88 + x^85 + x^83 + x^80 + x^79 + x^78 + x^77 + x^76 + x^75 + x^71 + x^67 + x^65 + x^62 + x^60 + x^59 + x^58 + x^57 + x^56 + x^55 + x^54 + x^52 + x^50 + x^49 + x^46 + x^45 + x^42 + x^41 + x^40 + x^38 + x^37 + x^33 + x^31 + x^30 + x^29 + x^28 + x^23 + x^22 + x^21 + x^16 + x^15 + x^14 + 1
 	
 	private static void next() {
 		final long t0 = s0;
 		long t1 = s1;
 		t1 ^= t0;
-		s0 = Long.rotateLeft(t0, A) ^ t1 ^ t1 << B;
-		s1 = Long.rotateLeft(t1, C);
+		s0 = Long.rotateLeft(t0, 55) ^ t1 ^ t1 << 14;
+		s1 = Long.rotateLeft(t1, 36);
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String[] shifts = scanner.next().split("-");
-		A = Integer.parseInt(shifts[0]);
-		B = Integer.parseInt(shifts[1]);
-		C = Integer.parseInt(shifts[2]);
-		scanner.next();
-		String polynomial = scanner.nextLine();
-		
-		
 		final long[] v0 = new long[2 * BITS];
 		final long[] v1 = new long[2 * BITS];
 		for(int i = 0; i < 2 * BITS; i++) {
@@ -37,7 +27,7 @@ public class GenerateDistancePolynomialsXoRoShiRo128 {
 			next();
 		}
 		
-		System.out.println("F.<x> = FiniteField(2^128, modulus=" + polynomial + ")");
+		System.out.println("F.<x> = FiniteField(2^128, modulus=x^128 + x^118 + x^117 + x^114 + x^112 + x^109 + x^108 + x^107 + x^106 + x^103 + x^102 + x^101 + x^99 + x^98 + x^96 + x^94 + x^93 + x^92 + x^91 + x^90 + x^89 + x^88 + x^85 + x^83 + x^80 + x^79 + x^78 + x^77 + x^76 + x^75 + x^71 + x^67 + x^65 + x^62 + x^60 + x^59 + x^58 + x^57 + x^56 + x^55 + x^54 + x^52 + x^50 + x^49 + x^46 + x^45 + x^42 + x^41 + x^40 + x^38 + x^37 + x^33 + x^31 + x^30 + x^29 + x^28 + x^23 + x^22 + x^21 + x^16 + x^15 + x^14 + 1)");
 		
 		for(int i = 0; i < 63; i++) {
 			final Modulo2System system = new Modulo2System(BITS);
@@ -54,4 +44,5 @@ public class GenerateDistancePolynomialsXoRoShiRo128 {
 			System.out.println(").log(x))");
 		}
 	}
+
 }
