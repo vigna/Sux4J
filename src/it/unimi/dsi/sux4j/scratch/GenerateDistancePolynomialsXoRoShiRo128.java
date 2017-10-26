03e301d854148421a0335cdd80410a0507ee92fd
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2017-10-26 18:10:28 +0200

    XOR and parallelization

--- src/it/unimi/dsi/sux4j/scratch/GenerateDistancePolynomialsXoRoShiRo128.java
+++ src/it/unimi/dsi/sux4j/scratch/GenerateDistancePolynomialsXoRoShiRo128.java
@@ -7,10 +7,10 @@ import it.unimi.dsi.sux4j.mph.solve.Modulo2System.Modulo2Equation;
 
 public class GenerateDistancePolynomialsXoRoShiRo128 {
 	public static int BITS = 128;
-	
+
 	private static long s0 = -1, s1;
 	private static int A, B, C;
-	
+
 	private static void next() {
 		final long t0 = s0;
 		long t1 = s1;
@@ -20,6 +20,7 @@ public class GenerateDistancePolynomialsXoRoShiRo128 {
 	}
 
 	public static void main(String[] args) {
+		final boolean xor = args.length > 0;
 		Scanner scanner = new Scanner(System.in);
 		String[] shifts = scanner.next().split("-");
 		A = Integer.parseInt(shifts[0]);
@@ -27,31 +28,36 @@ public class GenerateDistancePolynomialsXoRoShiRo128 {
 		C = Integer.parseInt(shifts[2]);
 		scanner.next();
 		String polynomial = scanner.nextLine();
-		
-		
+
+
 		final long[] v0 = new long[2 * BITS];
-		final long[] v1 = new long[2 * BITS];
 		for(int i = 0; i < 2 * BITS; i++) {
-			v0[i] = s0;
-			v1[i] = s1;
+			v0[i] = xor ? (s0 ^ s1) : s0;
 			next();
 		}
-		
+
 		System.out.println("F.<x> = FiniteField(2^128, modulus=" + polynomial + ")");
-		
+		System.out.println("@parallel");
+		System.out.println("def parlog(dummy,p): return (p).log(x)");
+		System.out.println("for v in sorted(list(parlog([");
+
 		for(int i = 0; i < 63; i++) {
 			final Modulo2System system = new Modulo2System(BITS);
 			for(int b = 0; b < BITS; b++) {
 				Modulo2Equation eq = new Modulo2Equation((v0[b] & 1L << i) != 0 ? 1 : 0, BITS);
-				for(int v = 0; v < BITS; v++) if ((v0[b+v] & 1L << i + 1) != 0) eq.add(v); 
+				for(int v = 0; v < BITS; v++) if ((v0[b+v] & 1L << i + 1) != 0) eq.add(v);
 				system.add(eq);
 			}
-			
+
 			final long[] solution = new long[BITS];
 			if (! system.lazyGaussianElimination(solution)) throw new IllegalStateException();
-			System.out.print("print((");
+
+			if (i != 0) System.out.println(",");
+			System.out.print("(" + i + ", ");
 			for(int e = 0; e < BITS; e++) if (solution[e] != 0) System.out.print(" + x^" + e);
-			System.out.println(").log(x))");
+			System.out.print(")");
 		}
+
+		System.out.println("]))): print(v[1])");
 	}
 }
