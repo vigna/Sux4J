58a7102b5805619075fed510f33e4ec661cbd266
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2017-11-08 00:42:12 +0100

    Fixed spacing

--- genz.sh
+++ genz.sh
@@ -2,10 +2,10 @@
 
 cp src/it/unimi/dsi/sux4j/bits/SimpleSelect.java src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 
-sed -i -e 's/bits\[ /~bits\[ /g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
+sed -i -e 's/bits\[\([^]]\)/~bits\[\1/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/Select/SelectZero/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/select(/selectZero(/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/Fast\.selectZero(/Fast\.select(/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e '/numOnes = c/i\
-if ( length % 64 != 0 ) c -= 64 - length % 64;' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
+if (length % 64 != 0) c -= 64 - length % 64;' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
 sed -i -e 's/select implementation/zero-select implementation/g' src/it/unimi/dsi/sux4j/bits/SimpleSelectZero.java
