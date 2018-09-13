a1771a3cb2d4ba702e83cc7374638fb75a7f18b8
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2018-09-13 10:16:06 +0200

    Generator for uniform values

--- src/it/unimi/dsi/sux4j/test/GenerateUniformValues.java
+++ src/it/unimi/dsi/sux4j/test/GenerateUniformValues.java
@@ -19,13 +19,14 @@ import it.unimi.dsi.fastutil.io.FastBufferedOutputStream;
 import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.util.XoRoShiRo128PlusRandomGenerator;
 
-public class GenerateGeometricValues {
-	public static final Logger LOGGER = LoggerFactory.getLogger(GenerateGeometricValues.class);
+public class GenerateUniformValues {
+	public static final Logger LOGGER = LoggerFactory.getLogger(GenerateUniformValues.class);
 
 	public static void main(final String[] arg) throws JSAPException, IOException {
 
-		final SimpleJSAP jsap = new SimpleJSAP(GenerateGeometricValues.class.getName(), "Generates a binary list of longs geometrically distributed.",
+		final SimpleJSAP jsap = new SimpleJSAP(GenerateUniformValues.class.getName(), "Generates a binary list of uniformly distributed longs using a given number of bits.",
 				new Parameter[] {
+					new UnflaggedOption("b", JSAP.INTEGER_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The number of bits."),
 					new UnflaggedOption("n", JSAP.LONG_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The number of longs."),
 					new UnflaggedOption("output", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The output file.")
 		});
@@ -33,18 +34,20 @@ public class GenerateGeometricValues {
 		final JSAPResult jsapResult = jsap.parse(arg);
 		if (jsap.messagePrinted()) return;
 
+		final int b = jsapResult.getInt("b");
 		final long n = jsapResult.getLong("n");
 		final String output = jsapResult.getString("output");
 
 		final RandomGenerator r = new XoRoShiRo128PlusRandomGenerator();
+		final long mask = b == 64 ? -1L: (1L << b) - 1;
 
 		final ProgressLogger pl = new ProgressLogger(LOGGER);
 		pl.expectedUpdates = n;
 		pl.start("Generating... ");
+
 		final DataOutputStream dos = new DataOutputStream(new FastBufferedOutputStream(new FileOutputStream(output)));
 
-		for(long i = 0; i < n; i++) dos.writeLong(Long.numberOfTrailingZeros(r.nextLong()));
-
+		for(long i = 0; i < n; i++) dos.writeLong(r.nextLong() & mask);
 		pl.done();
 		dos.close();
 	}
