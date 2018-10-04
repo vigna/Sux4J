a4e793dec6e7362e8207b903324357c65a973985
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2018-10-04 10:46:45 +0200

    Bumped dates and version number

--- src/it/unimi/dsi/sux4j/bits/RankSelect.java
+++ src/it/unimi/dsi/sux4j/bits/RankSelect.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 /*
  * Sux4J: Succinct data structures for Java
  *
- * Copyright (C) 2008-2017 Sebastiano Vigna
+ * Copyright (C) 2008-2018 Sebastiano Vigna
  *
  *  This library is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU Lesser General Public License as published by the Free
--- src/it/unimi/dsi/sux4j/mph/solve/Modulo2SparseSystem.java
+++ src/it/unimi/dsi/sux4j/mph/solve/Modulo2SparseSystem.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 /*
  * Sux4J: Succinct data structures for Java
  *
- * Copyright (C) 2015-2017 Sebastiano Vigna
+ * Copyright (C) 2015-2018 Sebastiano Vigna
  *
  *  This library is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU Lesser General Public License as published by the Free
