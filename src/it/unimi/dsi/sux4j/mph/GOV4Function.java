38dcf53cdca8180d6f7052c39e001dd85b50c5d9
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2019-05-17 12:10:17 -0700

    Max threads are now 4 by default

--- src/it/unimi/dsi/sux4j/mph/GOV4Function.java
+++ src/it/unimi/dsi/sux4j/mph/GOV4Function.java
@@ -138,7 +138,7 @@ import it.unimi.dsi.util.concurrent.ReorderingBlockingQueue;
  * <h2>Multithreading</h2>
  *
  * <p>This implementation is multithreaded: each bucket returned by the {@link BucketedHashStore} is processed independently. By
- * default, this class uses {@link Runtime#availableProcessors()} parallel threads, but never more than 8. If you wish to
+ * default, this class uses {@link Runtime#availableProcessors()} parallel threads, but by default no more than 4. If you wish to
  * set a specific number of threads, you can do so through the system property {@value #NUMBER_OF_THREADS_PROPERTY}.
  *
  * <h2>Implementation Details</h2>
@@ -429,7 +429,7 @@ public class GOV4Function<T> extends AbstractObject2LongFunction<T> implements S
 			final AtomicLong unsolvable = new AtomicLong();
 
 			try {
-				final int numberOfThreads = Integer.parseInt(System.getProperty(NUMBER_OF_THREADS_PROPERTY, Integer.toString(Math.min(8, Runtime.getRuntime().availableProcessors()))));
+				final int numberOfThreads = Integer.parseInt(System.getProperty(NUMBER_OF_THREADS_PROPERTY, Integer.toString(Math.min(4, Runtime.getRuntime().availableProcessors()))));
 				final ArrayBlockingQueue<Bucket> bucketQueue = new ArrayBlockingQueue<>(numberOfThreads * 8);
 				final ReorderingBlockingQueue<LongArrayBitVector> queue = new ReorderingBlockingQueue<>(numberOfThreads * 128);
 				final ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads + 2);
--- src/it/unimi/dsi/sux4j/mph/GV4CompressedFunction.java
+++ src/it/unimi/dsi/sux4j/mph/GV4CompressedFunction.java
@@ -132,7 +132,7 @@ import it.unimi.dsi.util.concurrent.ReorderingBlockingQueue;
  * <h2>Multithreading</h2>
  *
  * <p>This implementation is multithreaded: each bucket returned by the {@link BucketedHashStore} is processed independently. By
- * default, this class uses {@link Runtime#availableProcessors()} parallel threads, but never more than 8. If you wish to
+ * default, this class uses {@link Runtime#availableProcessors()} parallel threads, but by default no more than 4. If you wish to
  * set a specific number of threads, you can do so through the system property {@value #NUMBER_OF_THREADS_PROPERTY}.
  *
  * <h2>Implementation Details</h2>
@@ -433,7 +433,7 @@ public class GV4CompressedFunction<T> extends AbstractObject2LongFunction<T> imp
 			final AtomicLong unsolvable = new AtomicLong();
 
 			try {
-				final int numberOfThreads = Integer.parseInt(System.getProperty(NUMBER_OF_THREADS_PROPERTY, Integer.toString(Math.min(8, Runtime.getRuntime().availableProcessors()))));
+				final int numberOfThreads = Integer.parseInt(System.getProperty(NUMBER_OF_THREADS_PROPERTY, Integer.toString(Math.min(4, Runtime.getRuntime().availableProcessors()))));
 				final ArrayBlockingQueue<Pair<Bucket, Integer>> bucketQueue = new ArrayBlockingQueue<>(numberOfThreads);
 				final ReorderingBlockingQueue<LongArrayBitVector> queue = new ReorderingBlockingQueue<>(numberOfThreads * 128);
 				final ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads + 2);
