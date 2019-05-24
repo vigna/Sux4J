83e7ba1d1d524d6d729f92de4390c914a9a1d39b
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2019-05-23 17:54:39 -0700

    Added prediction

--- c/csf4.c
+++ c/csf4.c
@@ -66,7 +66,7 @@ int64_t csf4_get_byte_array(const csf *csf, char *key, uint64_t len) {
 	int e[3];
 	signature_to_equation(signature, offset_seed & ~OFFSET_MASK, num_variables, e);
 	const int64_t t = decode(csf, get_value(csf->array, e[0] + bucket_offset, w) ^ get_value(csf->array, e[1] + bucket_offset, w) ^ get_value(csf->array, e[2] + bucket_offset, w) ^ get_value(csf->array, e[3] + bucket_offset, w));
-	if (t != -1) return t;
+	if (__builtin_expect(t != -1, 1)) return t;
 	const uint64_t end = csf->global_max_codeword_length - csf->escape_length;
 	const uint64_t start = end - csf->escaped_symbol_length;
 	return get_value(csf->array, e[0] + start, e[0] + end) ^ get_value(csf->array, e[1] + start, e[1] + end) ^ get_value(csf->array, e[2] + start, e[2] + end) ^ get_value(csf->array, e[3] + start, e[3] + end);
@@ -82,5 +82,9 @@ int64_t csf4_get_uint64_t(const csf *csf, const uint64_t key) {
 	const int num_variables = (csf->offset_and_seed[bucket + 1] & OFFSET_MASK) - bucket_offset - w;
 	int e[3];
 	signature_to_equation(signature, offset_seed & ~OFFSET_MASK, num_variables, e);
-	return decode(csf, get_value(csf->array, e[0] + bucket_offset, w) ^ get_value(csf->array, e[1] + bucket_offset, w) ^ get_value(csf->array, e[2] + bucket_offset, w) ^ get_value(csf->array, e[3] + bucket_offset, w));
+	const int64_t t = decode(csf, get_value(csf->array, e[0] + bucket_offset, w) ^ get_value(csf->array, e[1] + bucket_offset, w) ^ get_value(csf->array, e[2] + bucket_offset, w) ^ get_value(csf->array, e[3] + bucket_offset, w));
+	if (__builtin_expect(t != -1, 1)) return t;
+	const uint64_t end = csf->global_max_codeword_length - csf->escape_length;
+	const uint64_t start = end - csf->escaped_symbol_length;
+	return get_value(csf->array, e[0] + start, e[0] + end) ^ get_value(csf->array, e[1] + start, e[1] + end) ^ get_value(csf->array, e[2] + start, e[2] + end) ^ get_value(csf->array, e[3] + start, e[3] + end);
 }
