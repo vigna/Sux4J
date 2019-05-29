1f3950fc6284c089231462248bb91ab0e38f4950
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2019-05-29 23:13:57 +0200

    Added -g

--- c/comp.sh
+++ c/comp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gcc $@ -O3 -march=native -fomit-frame-pointer test_mph_byte_array.c mph.c spooky.c -o test_mph_byte_array
-gcc $@ -O3 -march=native -fomit-frame-pointer test_mph_uint64_t.c mph.c spooky.c -o test_mph_uint64_t
+gcc $@ -O3 -g -march=native -fomit-frame-pointer test_mph_byte_array.c mph.c spooky.c -o test_mph_byte_array
+gcc $@ -O3 -g -march=native -fomit-frame-pointer test_mph_uint64_t.c mph.c spooky.c -o test_mph_uint64_t
 
-gcc $@ -O3 -march=native -fomit-frame-pointer test_sf3_byte_array.c sf.c sf3.c spooky.c -o test_sf3_byte_array
-gcc $@ -O3 -march=native -fomit-frame-pointer test_sf4_byte_array.c sf.c sf4.c spooky.c -o test_sf4_byte_array
+gcc $@ -O3 -g -march=native -fomit-frame-pointer test_sf3_byte_array.c sf.c sf3.c spooky.c -o test_sf3_byte_array
+gcc $@ -O3 -g -march=native -fomit-frame-pointer test_sf4_byte_array.c sf.c sf4.c spooky.c -o test_sf4_byte_array
 
-gcc $@ -O3 -march=native -fomit-frame-pointer test_csf3_byte_array.c csf.c csf3.c spooky.c -o test_csf3_byte_array
-gcc $@ -O3 -march=native -fomit-frame-pointer test_csf4_byte_array.c csf.c csf4.c spooky.c -o test_csf4_byte_array
+gcc $@ -O3 -g -march=native -fomit-frame-pointer test_csf3_byte_array.c csf.c csf3.c spooky.c -o test_csf3_byte_array
+gcc $@ -O3 -g -march=native -fomit-frame-pointer test_csf4_byte_array.c csf.c csf4.c spooky.c -o test_csf4_byte_array
