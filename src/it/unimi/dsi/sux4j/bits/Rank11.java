c677da481929a6c0c15b788f864c2d4b506ae4e2
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2020-12-08 19:11:27 +0100

    Modified license

--- src/it/unimi/dsi/sux4j/bits/Rank11.java
+++ src/it/unimi/dsi/sux4j/bits/Rank11.java
@@ -3,19 +3,18 @@
  *
  * Copyright (C) 2014-2020 Sebastiano Vigna
  *
- *  This library is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU Lesser General Public License as published by the Free
- *  Software Foundation; either version 3 of the License, or (at your option)
- *  any later version.
+ * This program and the accompanying materials are made available under the
+ * terms of the GNU Lesser General Public License v2.1 or later,
+ * which is available at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html,
+ * or the Apache Software License 2.0, which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
  *
- *  This library is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- *  for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses/>.
- *
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR Apache-2.0
  */
 
 package it.unimi.dsi.sux4j.bits;
