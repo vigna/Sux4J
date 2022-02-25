e9fd7412204272a2796e3038e95beb1d8cbc244a
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2022-02-25 09:19:01 +0100

    Bumped dates and revision

--- src/it/unimi/dsi/sux4j/bits/AbstractRank.java
+++ src/it/unimi/dsi/sux4j/bits/AbstractRank.java
@@ -1,7 +1,7 @@
 /*
  * Sux4J: Succinct data structures for Java
  *
- * Copyright (C) 2007-2021 Sebastiano Vigna
+ * Copyright (C) 2007-2022 Sebastiano Vigna
  *
  * This program and the accompanying materials are made available under the
  * terms of the GNU Lesser General Public License v2.1 or later,
